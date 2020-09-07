f6a7410bc8820a526ab0846474cbc828f5976d9c
heathkev <kevintuazon45@gmail.com>
2020-09-08 00:41:46 +0800

    Scheduled functions for trends

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
+const { firestore } = require('firebase-admin');
 admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -19,8 +20,27 @@ exports.logActivities = functions.firestore.document('/{collection}/{id}')
         const activities = admin.firestore().collection('activities');
 
         if(collection === 'Users'){
-            return activities.add({text: 'a new user was added'})
+            return activities.add({text: 'a new user was added' + id })
+        }
+
+        if(collection === 'Results'){
+            return activities.add({text: id + 'has completed a quiz' })
         }
 
         return null;
     })
+
+exports.scheduledFunction = functions.pubsub.schedule('every 5 minutes')
+    .onRun(async (snap, context) => {
+        console.log('This will be run every 5 minutes!');
+
+        // const feed = admin.firestore().collection('feed').collection('trends');
+
+        // const quizzes = admin.firestore().collection('Results')
+        // const collection = await quizzes.get();
+        
+        // collection.docs.forEach(doc => {
+        //     doc.get()
+            
+        // });
+    });
